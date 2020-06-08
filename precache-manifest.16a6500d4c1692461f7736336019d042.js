2ffb042af378c66b8d90b9f132e35dd9d5bce2bc
marco.lazzarini <marco.lazzarini@accenture.com>
2020-06-08 14:24:28 +0200

    Updates

---
+++ precache-manifest.16a6500d4c1692461f7736336019d042.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ee05e9a3222dd1187206d5f0819f47",
+    "url": "/ombrellone/index.html"
+  },
+  {
+    "revision": "3b2f0b59d8e46ed1fbcf",
+    "url": "/ombrellone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11fe050fb9a9963194ba",
+    "url": "/ombrellone/static/js/2.1f6f81c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ombrellone/static/js/2.1f6f81c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2f0b59d8e46ed1fbcf",
+    "url": "/ombrellone/static/js/main.e61c2519.chunk.js"
+  },
+  {
+    "revision": "d3c7f556f282fbbca1cf",
+    "url": "/ombrellone/static/js/runtime-main.74521488.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ombrellone/static/media/logo.5d5d9eef.svg"
+  }
+]);
